aa4ac5ecf386349fe39fc07fa2522c6a85fb460c
MiguelNunes <miguelnunesc29@gmail.com>
2025-03-23 16:32:34 -0300

    chat page create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Rota padrão
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'chat', component: ChatComponent },
   { path: '**', redirectTo: 'login' } // Rota curinga (404)
 ];
